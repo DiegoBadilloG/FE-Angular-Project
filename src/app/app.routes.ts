a6535b52fe4f6d5af0fd51989bba538720cf0e1a
Diego Andres Badillo Gomez <DiegoAndres.BadilloGomez@ust.com>
2024-04-30 14:31:45 +0200

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { RecipiesComponent } from './Features/recipies/recipies.component';
+import { HomeComponent } from './Features/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "recipies", component: RecipiesComponent},
+    { path: "home", component: HomeComponent},
+];
