6088d1c2a637cbc0f058c4b9dd8706b72025972c
Diego Andres Badillo Gomez <DiegoAndres.BadilloGomez@ust.com>
2024-05-08 13:12:10 +0200

    faq completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { RecipiesComponent } from './Features/recipies/recipies.component';
 import { HomeComponent } from './Features/home/home.component';
+import { FaqComponent } from './Features/faq/faq.component';
 
 export const routes: Routes = [
     { path: "recipies", component: RecipiesComponent},
     { path: "home", component: HomeComponent},
-    { path: "", redirectTo: "/home", pathMatch: "full"}
+    { path: "", redirectTo: "/home", pathMatch: "full"},
+    { path: "faq", component: FaqComponent}
 ];
