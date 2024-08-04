ff35fd852351648fe0fd0f6ee5a5ab106b11eee0
Diego Badillo <diegoabgomez@gmail.com>
2024-08-04 15:41:46 +0200

    Auto-generated commit

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-IGLPQSII.mjs";import"./chunk-32IW46MZ.mjs";import"./chunk-IVTEHD23.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-XGRIXZAI.mjs";import"./chunk-32IW46MZ.mjs";import"./chunk-IVTEHD23.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
