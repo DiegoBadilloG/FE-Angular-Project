dac55365c8a8bed082b6cd956cda5d5573c1752f
Diego Badillo <diegoabgomez@gmail.com>
2024-09-19 17:37:23 +0200

    added sliders to e-commercial

--- src/app/Features/e-commercial/e-commercial.component.ts
+++ src/app/Features/e-commercial/e-commercial.component.ts
@@ -1,9 +1,11 @@
 import { UrlBaseService } from './../../Services/url-base.service';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-e-commercial',
@@ -17,7 +19,7 @@ import { CommonModule } from '@angular/common';
   templateUrl: './e-commercial.component.html',
   styleUrl: './e-commercial.component.css'
 })
-export class ECommercialComponent {
+export class ECommercialComponent implements OnInit {
   imgRoute: string = "";
 
   eshopMainBtn: any;
@@ -29,8 +31,41 @@ export class ECommercialComponent {
   displaySport: boolean = false;
   private leaveTimeout!: any;
 
-  constructor(private urlBaseService: UrlBaseService) {
+  private eShopUrl: string = "/e-commercial.json";
+  private fullUrl: string = "";
+  mainBanner: any; 
+  newElems: any;
+  bestElems: any;
+  soonElems: any;
+  sldElems: any;
+  newBtn: boolean = false;
+  bestBtn: boolean = false;
+  soonBtn: boolean = false;
+  private prevSliderBtnSelected: string = "";
+
+  constructor(@Inject(PLATFORM_ID) private platformId: any, private urlBaseService: UrlBaseService, private httpService: HttpClient) {
     this.imgRoute = this.urlBaseService.getImgUrl();
+    this.fullUrl = this.urlBaseService.getApiUrl() + this.eShopUrl;
+
+    this.httpService.get(this.fullUrl).subscribe((data: any) => {
+      this.newElems = data.eCommercialHome.newElems;
+      this.mainBanner = data.eCommercialHome.banerElems;
+      this.bestElems = data.eCommercialHome.theBestElems;
+      this.soonElems = data.eCommercialHome.soonElems;
+      this.sldElems = data.eCommercialHome.sliderElems;
+    })
+  }
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      alert("Página en construcción. Algunas funcionalidades pueden no estar disponibles. ¡Gracias por tu comprensión!");
+      let selectedBtn = document.getElementById('newBtn');
+      if (selectedBtn) {
+        selectedBtn.classList.add('active');
+      }
+      this.newBtn = true;
+      this.prevSliderBtnSelected = 'newBtn';
+    }
   }
 
   showElem(elem: any) {
@@ -81,6 +116,43 @@ export class ECommercialComponent {
           this.displaySport = false;
           break;
       }  
-    }, );
+    }, 1);
   }
+
+  toggleSliderBtn(elem: any, elemId: string) {
+    this.toggleStyleSliderBtn(this.prevSliderBtnSelected);
+    this.prevSliderBtnSelected = elem;
+    switch(elem) {
+      case 'newBtn':
+        this.newBtn = true;
+        this.bestBtn = false;
+        this.soonBtn = false;
+        break;
+      case 'bestBtn':
+        this.newBtn = false;
+        this.bestBtn = true;
+        this.soonBtn = false;
+        break;
+      case 'soonBtn':
+        this.newBtn = false;
+        this.bestBtn = false;
+        this.soonBtn = true;
+        break;
+      default: 
+        null;
+    }
+    this.toggleStyleSliderBtn(elemId);
+  }
+
+  toggleStyleSliderBtn(elemId: string) {
+    let item = document.getElementById(elemId);
+    if (item?.classList.contains('active')) {
+      item.classList.remove('active')
+    }
+    else {
+      item?.classList.add('active');
+    }
+    
+  }
+
 }
